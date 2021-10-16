183f4c06d348d04ae7c0f2756096afa94d6175a6
tamberg <tamberg@yaler.net>
2021-10-16 18:20:09 +0200

    Update.

--- 05/Arduino/nRF52840Sense_DigitalReadSerial/nRF52840Sense_DigitalReadSerial.ino
+++ 05/Arduino/nRF52840Sense_DigitalReadSerial/nRF52840Sense_DigitalReadSerial.ino
@@ -1,14 +1,16 @@
+#include "Adafruit_TinyUSB.h"; // Fix https://github.com/adafruit/Adafruit_nRF52_Arduino/issues/653
+
 //int sensorPin = 9; // external, Grove D4
 int sensorPin = 7; // onboard, user button
 
 void setup() { // called once
   //pinMode(sensorPin, INPUT); // external
-  pinMode(sensorPin, INPUT_PULLUP);
+  pinMode(sensorPin, INPUT_PULLUP); // onboard
   Serial.begin(9600);
 }
 
 void loop() { // called in a loop
-  int value = digitalRead(pin);
+  int value = digitalRead(sensorPin);
   Serial.println(value);
   delay(100); // ms
 }
