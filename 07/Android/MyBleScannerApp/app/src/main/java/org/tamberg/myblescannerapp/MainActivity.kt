0cf9bba39cca7281a455ddede18cbce3d770b375
tamberg <tamberg@yaler.net>
2023-10-29 19:22:08 +0100

    Update.

--- 07/Android/MyBleScannerApp/app/src/main/java/org/tamberg/myblescannerapp/MainActivity.kt
+++ 07/Android/MyBleScannerApp/app/src/main/java/org/tamberg/myblescannerapp/MainActivity.kt
@@ -1,23 +1,22 @@
 package org.tamberg.myblescannerapp
 
 import android.Manifest
-import android.annotation.SuppressLint
+import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.content.Context.BLUETOOTH_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,17 +26,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.tamberg.myblescannerapp.ui.theme.MyBleScannerAppTheme
 
+val LocalBlePermissionHelper = staticCompositionLocalOf<MyBlePermissionHelper> { null!! }
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val helper = MyBlePermissionHelper(this) // TODO
         setContent {
             MyBleScannerAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -45,7 +50,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyBleScannerView()
+                    CompositionLocalProvider(LocalBlePermissionHelper provides helper) {
+                        MyBleScannerView()
+                    }
                 }
             }
         }
@@ -77,44 +84,132 @@ fun MyBleScannerView(model: MyBleScannerViewModel = viewModel()) {
     }
 }
 
-class MyBleScannerViewModel() : ViewModel() {
-    var info = mutableStateOf<String>("Scan for peripherals.")
-    var command = mutableStateOf<String>("Scan")
-    var enabled = mutableStateOf<Boolean>(true)
+class MyBlePermissionHelper(private val activity: ComponentActivity) {
+
+    private val startActivityForResultLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        //Log.d(TAG, "result.resultCode = ${result.resultCode}"); // TODO
+    }
+
+    fun askToEnableBle() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResultLauncher.launch(enableBtIntent)
+    }
+
+    fun askToEnableLocation() {
+        val enableIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivityForResultLauncher.launch(enableIntent)
+    }
+
+    // TODO
+    // https://stackoverflow.com/questions/66551781/android-onrequestpermissionsresult-is-deprecated-are-there-any-alternatives
+    //fun onRequestPermissionsResult(
+    //    requestCode: Int,
+    //    permissions: Array<String?>?,
+    //    grantResults: IntArray?
+    //) {
+    //     ...
+    //}
+
+     private val requestPermissionLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            // TODO
+        }
+    }
+
+    private val requestMultiplePermissionsLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        permissions.entries.forEach {
+            //Log.d(TAG, "${it.key} = ${it.value}") // TODO
+        }
+    }
+
+    fun askForPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+           requestMultiplePermissionsLauncher.launch(arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT))
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+}
+
+class MyBleScannerViewModel(application: Application) : AndroidViewModel(application) {
+    val helper = LocalBlePermissionHelper.current // TODO: geht nur in Composable
+    val info = mutableStateOf<String>("Scan for peripherals.")
+    val command = mutableStateOf<String>("Scan")
+    val enabled = mutableStateOf<Boolean>(true)
 
     private fun hasBle(): Boolean {
-        //val hasBle = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
-        return false
+        val app = super.getApplication<Application>()
+        return app.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
     }
 
     private fun isBleEnabled(): Boolean {
-        // TODO
-        return false
+        val app = super.getApplication<Application>()
+        val bluetoothManager = app.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        return bluetoothAdapter != null && bluetoothAdapter.isEnabled
     }
 
-    private fun askToEnableBle() {
-        // TODO
-    }
     private fun isLocationEnabled(): Boolean {
         // TODO
         return false
     }
 
-    private fun askToEnableLocation() {
-        // TODO
-    }
-
     private fun hasPermission(): Boolean {
-        // TODO
-        return false
+        val result: Boolean
+        val app = super.getApplication<Application>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            result = (app.checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) &&
+                    (app.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            result = app.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            result = app.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        } else {
+            result = false // TODO
+        }
+        return result
     }
 
-    private fun askForPermission() {
-        // TODO
+    //private val REQUEST_ENABLE_BT = 1
+    //private val SCAN_PERIOD_MS: Long = 10000
+    //private var scanner: BluetoothLeScanner? = null
+    //private val handler: Handler? = Handler(Looper.getMainLooper())
+
+    private val scanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            //Log.d(TAG, "onScanResult, result = " + result.device.address) // TODO
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            //Log.d(TAG, "onScanFailed, errorCode = $errorCode") // TODO
+        }
     }
 
     private fun doScan() {
+        val app = super.getApplication<Application>()
+        val bluetoothManager = app.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        val scanner = bluetoothAdapter.bluetoothLeScanner
         // TODO
+        //assert(handler != null)
+        assert(scanner != null)
+        //handler!!.postDelayed({
+        //    Log.d(TAG, "stop scan")
+        //    scanner!!.stopScan(scanCallback)
+        //}, SCAN_PERIOD_MS)
+        //Log.d(TAG, "start scan")
+        //scanner!!.startScan(scanCallback)
     }
 
     fun scan() {
@@ -126,17 +221,17 @@ class MyBleScannerViewModel() : ViewModel() {
             info.value = "Bluetooth not enabled."
             command.value = "Enable Bluetooth"
             enabled.value = true
-            askToEnableBle()
+            helper.askToEnableBle() // TODO
         } else if (!hasPermission()) {
             info.value = "Permission not given."
-            command.value = "Request permission"
+            command.value = "Give permission"
             enabled.value = true
-            askForPermission()
+            helper.askForPermission() // TODO
         } else if (!isLocationEnabled()) {
             info.value = "Location not enabled."
             command.value = "Enable location"
             enabled.value = true
-            askToEnableLocation()
+            helper.askToEnableLocation() // TODO
         } else {
             info.value = "Scan for peripherals."
             command.value = "Scan"
@@ -145,130 +240,3 @@ class MyBleScannerViewModel() : ViewModel() {
         }
     }
 }
-
-/*
-class MainActivity : ComponentActivity() {
-    private val TAG = MainActivity::class.java.simpleName
-    private val REQUEST_ENABLE_BT = 1
-    private val SCAN_PERIOD_MS: Long = 10000
-
-    private var scanner: BluetoothLeScanner? = null
-    private val handler: Handler? = Handler(Looper.getMainLooper())
-
-    private val scanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            Log.d(TAG, "onScanResult, result = " + result.device.address)
-        }
-
-        override fun onScanFailed(errorCode: Int) {
-            Log.d(TAG, "onScanFailed, errorCode = $errorCode")
-        }
-    }
-
-    @SuppressLint("MissingPermission") // TODO remove?
-    private fun scan() {
-        assert(handler != null)
-        assert(scanner != null)
-        handler!!.postDelayed({
-            Log.d(TAG, "stop scan")
-            scanner!!.stopScan(scanCallback)
-        }, SCAN_PERIOD_MS)
-        Log.d(TAG, "start scan")
-        scanner!!.startScan(scanCallback)
-    }
-
-    /* https://stackoverflow.com/questions/66551781/android-onrequestpermissionsresult-is-deprecated-are-there-any-alternatives
-    fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String?>?,
-        grantResults: IntArray?
-    ) {
-        scan()
-    }
-     */
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        Log.d(TAG, "isGranted = ${isGranted}")
-        if (isGranted) {
-            //scan() // TODO
-        }
-    }
-
-    private val requestMultiplePermissionsLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        permissions.entries.forEach {
-            Log.d(TAG, "${it.key} = ${it.value}") // TODO
-        }
-        //scan() // TODO
-    }
-
-    private val startActivityForResultLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        Log.d(TAG, "result.resultCode = ${result.resultCode}"); // TODO
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
-        setContent {
-            MyBleScannerAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ScanButton()
-                }
-            }
-        }
-
-        // TODO: refactor this part
-        val hasBle = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
-        // Or <uses-feature android:name="android.hardware.bluetooth_le" android:required="true"/>
-        if (hasBle) {
-            Log.d(TAG, "BLE available")
-            val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
-            val bluetoothAdapter = bluetoothManager.adapter
-            if (bluetoothAdapter != null && bluetoothAdapter.isEnabled) {
-                Log.d(TAG, "BLE enabled")
-                scanner = bluetoothAdapter.bluetoothLeScanner
-                /* https://stackoverflow.com/questions/66551781/android-onrequestpermissionsresult-is-deprecated-are-there-any-alternatives
-                val permissions = arrayOf<String>(
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                ActivityCompat.requestPermissions(this@MainActivity, permissions, 0)
-                */
-                // TODO: check permissions, only ask if not given?
-                // TODO: request permissions for other OS versions?
-                // TODO: ask user to adapt location permission
-                //val enableIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                //startActivityForResultLauncher2.launch(enableIntent)
-                scan() // TODO
-            } else {
-                Log.d(TAG, "BLE not enabled")
-                // https://stackoverflow.com/questions/68095709/startactivityforresult-is-deprecated-and-unable-to-request-for-bluetooth-connec
-                // TODO: refactor to allow multiple tries
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    requestMultiplePermissionsLauncher.launch(arrayOf(
-                        Manifest.permission.BLUETOOTH_SCAN,
-                        Manifest.permission.BLUETOOTH_CONNECT))
-                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                } else {
-                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                }
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                // startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                startActivityForResultLauncher.launch(enableBtIntent)
-            }
-        } else {
-            Log.d(TAG, "BLE not available")
-        }
-    }
-}
-*/
