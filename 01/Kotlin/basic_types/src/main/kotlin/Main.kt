777cd047bdc219baf9fb1daeed214a3c01db1b50
Thomas Amberg <tamberg@yaler.net>
2025-09-11 22:08:17 +0200

    Create Main.kt

---
+++ 01/Kotlin/basic_types/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+// https://pl.kotl.in/rKf_sSVpD
+
+fun main() {
+    val i = 3 // inferred type, initialized
+    println(i)
+    println(i is Int)
+    
+    val j: Int // explicit type, not initialized
+    //println(j) // error: Variable 'j' must be initialized.
+    j = 4 // late initialization of value
+    println(j)
+    
+    val k: Int = 5 // explicit type, redundant
+    println(k)
+}
