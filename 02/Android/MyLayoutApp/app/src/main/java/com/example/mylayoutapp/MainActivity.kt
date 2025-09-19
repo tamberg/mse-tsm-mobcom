8fa303f7bfb94c7418ba999662d50392a7a8c5df
tamberg <tamberg@yaler.net>
2025-09-19 14:13:50 +0200

    Update.

--- 02/Android/MyLayoutApp/app/src/main/java/com/example/mylayoutapp/MainActivity.kt
+++ 02/Android/MyLayoutApp/app/src/main/java/com/example/mylayoutapp/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -55,5 +56,13 @@ fun Greetings(
         for (name in names) { // this is fine
             Greeting(name, modifier)
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaterialTheme {
+        Greeting(name = "Android")
     }
 }
