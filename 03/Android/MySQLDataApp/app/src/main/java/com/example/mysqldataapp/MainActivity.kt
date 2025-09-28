c4bbeb31f9b4753ecc6c3ff735dcc9cda9045ae7
tamberg <tamberg@yaler.net>
2025-09-28 22:11:04 +0200

    Update.

--- 03/Android/MySQLDataApp/app/src/main/java/com/example/mysqldataapp/MainActivity.kt
+++ 03/Android/MySQLDataApp/app/src/main/java/com/example/mysqldataapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mysqldataapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
@@ -13,16 +12,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -39,13 +36,10 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.mysqldataapp.MyViewModel
 import com.example.mysqldataapp.ui.theme.MySQLDataAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -208,7 +202,7 @@ fun TopBar(onBack: () -> Unit, modifier: Modifier = Modifier) {
     ) {
         Row() {
             IconButton(onClick = onBack) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
             }
             Spacer(modifier = Modifier.weight(1.0f))
         }
