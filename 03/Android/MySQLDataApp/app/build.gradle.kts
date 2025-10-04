24b198305a09beb70739221564f26e07c9e5f2c7
tamberg <tamberg@yaler.net>
2025-10-04 22:22:42 +0200

    Update.

--- 03/Android/MySQLDataApp/app/build.gradle.kts
+++ 03/Android/MySQLDataApp/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // https://developer.android.com/build/migrate-to-ksp#add-ksp
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -57,4 +59,16 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // https://developer.android.com/training/data-storage/room/#kts
+    // https://developer.android.com/jetpack/androidx/releases/room
+    val room_version = "2.8.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    // https://developer.android.com/build/migrate-to-ksp#add-ksp
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:${room_version}")
+
+    //implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    //ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    //implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
