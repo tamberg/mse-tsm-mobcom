dc152f90beb5e5d93e81a83333ea9046bf49943d
tamberg <tamberg@yaler.net>
2025-10-09 22:40:02 +0200

    Add.

---
+++ 03/Android/MyCSVExportApp/app/src/main/java/com/example/mycsvexportapp/MainActivity.kt
@@ -0,0 +1,213 @@
+package com.example.mycsvexportapp
+
+import android.app.Activity
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mycsvexportapp.ui.theme.MyCSVExportAppTheme
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyCSVExportAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ListScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+const val CREATE_FILE = 1
+
+// https://developer.android.com/reference/android/arch/lifecycle/AndroidViewModel (deprecated)
+// https://developer.android.com/reference/androidx/lifecycle/AndroidViewModel
+// https://developer.android.com/reference/androidx/lifecycle/AndroidViewModelKt
+// https://developer.android.com/training/basics/intents/result
+// https://stackoverflow.com/questions/69075984/accessing-rememberlauncherforactivityresult-from-util-jetpack-composee ?
+// https://stackoverflow.com/questions/61455381/how-to-replace-startactivityforresult-with-activity-result-apis#61573632
+// https://stackoverflow.com/questions/78595106/start-intent-from-viewmodel
+class ListViewModel(app: Application): AndroidViewModel(app) {
+    val list = listOf(
+        Person(0, "Adele", "Goldberg", "Smalltalk"),
+        Person(1, "Niklaus", "Wirth", "Pascal"),
+        Person(2, "Dennis", "Ritchie", "C")
+    ).toMutableList()
+
+    val exportToCsvIntent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+        addCategory(Intent.CATEGORY_OPENABLE)
+        type = "text/csv" // mime type
+        putExtra(Intent.EXTRA_TITLE, "export.csv")
+    }
+
+    var launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val uri = result.data?.data
+            if (uri != null) viewModel.exportToCsv(uri)
+        }
+    }
+
+    fun exportToCsv(uri: Uri) {
+        val resolver = getApplication<Application>().contentResolver
+        try {
+            resolver.openOutputStream(uri)?.use { out ->
+                // https://www.rfc-editor.org/rfc/rfc4180
+                out.bufferedWriter(charset = Charsets.US_ASCII).use { writer ->
+                    writer.write("Name, Surname\n")
+                    for (person in list) {
+                        writer.write("${person.name}, ${person.surname}\n")
+                    }
+                }
+            }
+        } catch (e: FileNotFoundException) {
+        } catch (e: IOException) {
+        }
+    }
+}
+
+data class Person(
+    val id: Int,
+    val name: String,
+    val surname: String,
+    val language: String,
+)
+
+@Composable
+fun ListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ListViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        TopBar()
+        LazyColumn(
+            modifier = Modifier.padding(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = viewModel.list,
+                key = { person: Person -> person.id }
+            ) { person: Person ->
+                ListItem(person = person)
+            }
+        }
+//        var launcher = rememberLauncherForActivityResult(
+//            contract = ActivityResultContracts.StartActivityForResult()
+//        ) { result ->
+//            if (result.resultCode == Activity.RESULT_OK) {
+//                val uri = result.data?.data
+//                if (uri != null) viewModel.exportToCsv(uri)
+//            }
+//        }
+        Button(onClick = {
+             launcher.launch(viewModel.exportToCsvIntent)
+        }) { Text("Export") }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListScreenPreview() {
+    MyCSVExportAppTheme {
+        ListScreen()
+    }
+}
+
+@Composable
+fun ListItem(person: Person, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+        ) {
+            Text(text = "${person.name} ${person.surname}",
+                modifier.padding(horizontal = 8.dp))
+            Spacer(modifier = Modifier
+                .weight(1.0f)
+                .height(48.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PersonListItemPreview(
+    person: Person = Person(0, "Name", "Surname", "Language")
+) {
+    MyCSVExportAppTheme {
+        ListItem(person)
+    }
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primaryContainer,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row() {
+            IconButton(onClick = {}) {
+                Icon(Icons.Filled.Home, contentDescription = "Home")
+            }
+            Spacer(modifier = Modifier.weight(1.0f))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    MyCSVExportAppTheme {
+        TopBar()
+    }
+}
